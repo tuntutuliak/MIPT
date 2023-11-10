93ea3adc0a6630c1c9192f85b5f5c00ae8d9124b
evdokimov alexandr <dunupi@mail.ru>
2023-11-10 19:17:24 +1000

    H2 Database Added

--- examplatform/src/main/java/info/bitcom/examplatform/models/User.java
+++ examplatform/src/main/java/info/bitcom/examplatform/models/User.java
@@ -1,4 +1,5 @@
 package info.bitcom.examplatform.models;
+
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
